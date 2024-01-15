92cb9dc53bbfddb9a04727032c0ceaee2d60c9e6
Nicolas Alancay <nico.alan.alb@gmail.com>
2024-01-15 15:34:43 -0300

    Repository para interactuar con los departments de la BD.
    En Spring Data JPA existen interfaces, como "CrudRepository" y "JpaRepository", que al extenderlas nos proveen operaciones con la BD ya implementadas (ej. CRUD).
    
    Usamos una interfaz para extender JpaRepository para que podamos tener distintas implementaciones de la misma, y para poder agregar operaciones más específicas (aquellas que no nos provea Spring Data JPA).
    
    IMPORTANTE: los repositorios deben interactuar con los servicios y NO directamente con los controladores.

---
+++ src/main/java/com/nico/springbootprueba/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.nico.springbootprueba.repository;
+
+import com.nico.springbootprueba.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
