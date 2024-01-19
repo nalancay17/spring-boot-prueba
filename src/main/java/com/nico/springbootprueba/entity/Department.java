3fb9747917a0a8f86f72036f81217ad58288a0d2
Nicolas Alancay <nico.alan.alb@gmail.com>
2024-01-19 17:30:14 -0300

    Lombok para creación getters, setters, constructores, toString en tiempo de compilación.
    Se agregó la dependencia y se indicó que en el proyecto se utiliza el plugin de Lombok.

--- src/main/java/com/nico/springbootprueba/entity/Department.java
+++ src/main/java/com/nico/springbootprueba/entity/Department.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "department")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 
     @Id
@@ -25,56 +31,4 @@ public class Department {
     @Column(name = "code")
     private String code;
 
-    public Department() {
-
-    }
-
-    public Department(int id, String name, String address, String code) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.code = code;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", code='" + code + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/nico/springbootprueba/entity/ErrorMessage.java
+++ src/main/java/com/nico/springbootprueba/entity/ErrorMessage.java
@@ -1,7 +1,13 @@
 package com.nico.springbootprueba.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
 
     private String timestamp;
@@ -9,56 +15,4 @@ public class ErrorMessage {
     private HttpStatus error;
     private String message;
 
-    public ErrorMessage() {
-
-    }
-
-    public ErrorMessage(String timestamp, int status, HttpStatus error, String message) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "ErrorMessage{" +
-                "timestamp=" + timestamp +
-                ", status=" + status +
-                ", error=" + error +
-                ", message='" + message + '\'' +
-                '}';
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public HttpStatus getError() {
-        return error;
-    }
-
-    public void setError(HttpStatus error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
